c86a0b438be391311eda6e4f3c26fa5e02654a82
karadagserhat <serhatkaradagg@outlook.com>
2023-08-21 10:13:00 +0300

    first commit

---
+++ 13-strapi-submenus/src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Sibebar from './Sibebar';
+import Submenu from './Submenu';
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sibebar />
+      <Submenu />
+    </main>
+  );
+};
+export default App;
